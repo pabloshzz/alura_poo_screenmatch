cafa6fe00a5af6bdb95814f8ff877558f9fd8662
Pablo <pablosantosraf@gmail.com>
2025-06-22 17:40:11 -0300

    aprendendo o conceito de herança

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.models.Film;
+import br.com.alura.screenmatch.models.Series;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +11,19 @@ public class Main {
         myFilm.setDurationInMinutes(180);
 
 
-        myFilm.showDetails();
+       // myFilm.showDetails();
         myFilm.rating(5);
         myFilm.rating(7);
         myFilm.rating(10);
 
-        System.out.println("A média de avaliações do filme é: " + myFilm.getMedianRating());
+        //System.out.println("A média de avaliações do filme é: " + myFilm.getMedianRating());
+
+        Series mySeries = new Series();
+        mySeries.setName("Arcane");
+        mySeries.setGenre("Aventura");
+        mySeries.setYearOfRelease(2021);
+        mySeries.setSeasons(2);
+        mySeries.setEpisodesPerSeason(9);
+        mySeries.showDetails();
     }
 }
---
+++ src/br/com/alura/screenmatch/models/Title.java
@@ -0,0 +1,76 @@
+package br.com.alura.screenmatch.models;
+
+public class Title {
+    private String name, genre;
+    private int yearOfRelease;
+    private int durationInMinutes;
+    private int totalRatings;
+    private boolean incluidInPlan;
+    private double sumOfRatings;
+
+    public void showDetails() {
+        System.out.println("Nome do filme/série: " + name);
+        System.out.println("Ano de lançamento: " + yearOfRelease);
+        System.out.println("Gênero: " + genre);
+    }
+
+    public void rating(double rate) {
+        sumOfRatings += rate;
+        totalRatings++;
+    }
+
+    public double getMedianRating() {
+        return (sumOfRatings / totalRatings);
+    }
+
+    public int getTotalRatings() {
+        return totalRatings;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfRelease() {
+        return this.yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean getIncluidInPlan() {
+        return this.incluidInPlan;
+    }
+
+    public void setIncluidInPlan(boolean incluidInPlan) {
+        this.incluidInPlan = incluidInPlan;
+    }
+
+    public int getDurationInMinutes() {
+        return this.durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public double getSumOfRatings() {
+        return sumOfRatings;
+    }
+
+
+}
+
