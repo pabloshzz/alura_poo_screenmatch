a1993d0b7485d1f372b57b623df27199fb7ecad1
Pablo <pablosantosraf@gmail.com>
2025-06-22 18:10:28 -0300

    Aprendendo sobrescrever métodos com base apenas no nome

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,15 @@ public class Main {
         myFilm.setGenre("Terror");
         myFilm.setIncluidInPlan(true);
         myFilm.setDurationInMinutes(180);
+        myFilm.showDetails();
+        System.out.println("A duração do filme " + myFilm.getName() + " em minutos é: " + myFilm.getDurationInMinutes());
 
-
-       // myFilm.showDetails();
         myFilm.rating(5);
         myFilm.rating(7);
         myFilm.rating(10);
 
-        //System.out.println("A média de avaliações do filme é: " + myFilm.getMedianRating());
+        System.out.println("A média de avaliações do filme " + myFilm.getName() + " é: " + myFilm.getMedianRating());
+        System.out.println();
 
         Series mySeries = new Series();
         mySeries.setName("Arcane");
@@ -24,6 +25,8 @@ public class Main {
         mySeries.setYearOfRelease(2021);
         mySeries.setSeasons(2);
         mySeries.setEpisodesPerSeason(9);
+        mySeries.setMinutesPerEpisode(40);
         mySeries.showDetails();
+        System.out.println("O tempo para maratonar " + mySeries.getName() + " em minutos é: " + mySeries.getDurationInMinutes());
     }
 }
